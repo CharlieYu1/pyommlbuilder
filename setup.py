fa2b2385e3ef28692660423cd6663185e38d80c9
CharlieYu1 <imo.charlie@gmail.com>
2022-08-04 20:48:01 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pyommlbuilder",
+    version="0.0.1",
+    description="A wrapper of OMML using Python Objects",
+    author="Charlie Yu",
+    author_email="imo.charlie@gmail.com",
+)
