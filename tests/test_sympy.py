3b189f3c35e4df571633910723f2d6c166556c19
CharlieYu1 <imo.charlie@gmail.com>
2022-08-16 01:38:04 +0100

    removed sympy

--- tests/test_sympy.py
+++
@@ -1,8 +0,0 @@
-from sympy.abc import x, y
-from sympy import StrPrinter
-
-from pyommlbuilder.main import Run, Text
-
-def test_simple_sympy_expression():
-    expr = Run(Text(x+y))
-    assert expr._render_to_omml() == "<m:r><m:t>x + y</m:t></m:r>"
